7150b8fed195d6a082115b9837397c58b482ff0b
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-23 09:16:01 +0900

    Inference Test

--- pysrc/regression/frame_infer.py
+++ pysrc/regression/frame_infer.py
@@ -184,9 +184,9 @@ class FrameInfer:
             print("Period [s]: ", time.time() - start_clock)
             print("---------------------")
 
-            cv2.imshow('image',image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            #cv2.imshow('image',image)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
 
             infer_count += 1
 
